e06bb7030cb03800812987c906cc9540b737ac46
vanessuniq <vanessuniq@gmail.com>
2020-07-05 17:56:40 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,32 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    
+    if params[:artist_id]
+      @artist = find_instance(Artist, params[:artist_id])
+      if @artist
+        @songs = @artist.songs
+      else
+        flash[:alert] = 'Artist not found'
+        redirect_to artists_path
+      end
+      
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    @song = find_instance(Song, params[:id])
+    if @song
+      @song 
+    elsif params[:artist_id]
+      flash[:alert] = 'Song not found'
+      redirect_to artist_songs_path(params[:artist_id])
+    else
+      flash[:alert] = 'Song not found'
+      redirect_to songs_path
+    end
+    
   end
 
   def new
@@ -48,6 +70,10 @@ class SongsController < ApplicationController
 
   def song_params
     params.require(:song).permit(:title, :artist_name)
+  end
+
+  def find_instance(klass, id)
+    klass.find_by(id: id)
   end
 end
 
